20b62445fa679b579fc8dc4b6770ec26c8f21cc7
Heitor <Heitor@HEITOR>
2023-09-18 20:09:04 -0300

    .

--- sending-inputs-from-webclient.cpp
+++ sending-inputs-from-webclient.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
-
 int main()
 {
+	int webgui = 0;
+
+	if (webgui == 0) {
+		std::cout << "Connected" << std::endl; // Connected
+	} else {
+		std::cout << "Not Connected" << std::endl; // Not Connected
+	}
 
 }
