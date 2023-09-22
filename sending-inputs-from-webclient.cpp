d2ea64252423284998c3807f177173ddde3a956e
Heitor <Heitor@HEITOR>
2023-09-22 19:46:42 -0300

    .

--- sending-inputs-from-webclient.cpp
+++ sending-inputs-from-webclient.cpp
@@ -5,42 +5,65 @@
 using namespace std;
 
 int main() {
-    // Create a socket
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == INVALID_SOCKET) {
-        return 1;
-    }
+	// Create a WebSocket server socket
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET) {
+		return 1;
+	}
 
-    // Convert the IP address to a binary format
-    in_addr addr;
-    if (inet_pton(AF_INET, "google.com", &addr) != 1) {
-        // Handle error
-    }
+	// Bind the socket to a port
+	sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(8080);
+	addr.sin_addr.s_addr = INADDR_ANY;
 
-    // Connect to the server
-    sockaddr_in addr_in;
-    addr_in.sin_family = AF_INET;
-    addr_in.sin_port = htons(80); // Default port for HTTP
-    addr_in.sin_addr = addr;
+	bind(sock, (sockaddr*)&addr, sizeof(addr));
 
-    if (connect(sock, (sockaddr*)&addr_in, sizeof(addr_in)) == SOCKET_ERROR) {
-        return 1;
-    }
+	// Listen for incoming connections
+	listen(sock, SOMAXCONN);
 
-    // Send a request to the server
-    char request[] = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n";
-    send(sock, request, sizeof(request), 0);
+	// Start the WebSocket server loop
+	while (true) {
+		// Accept incoming connections
+		SOCKET client_sock = accept(sock, NULL, NULL);
+		if (client_sock == INVALID_SOCKET) {
+			break;
+		}
 
-    // Receive the response from the server
-    char buf[1024];
-    int n = recv(sock, buf, sizeof(buf), 0);
-    if (n > 0) {
-        // Process the response from the server
-        // ...
-    }
+		// Handle the WebSocket connection
+		// ...
+	}
 
-    // Close the socket
-    closesocket(sock);
+	// Convert the IP address to a binary format
+	in_addr addr1;
+	if (inet_pton(AF_INET, "sendinginputs.github.io", &addr1) != 1) {
+		// Handle error
+	}
 
-    return 0;
+	// Connect to the server
+	sockaddr_in addr_in;
+	addr_in.sin_family = AF_INET;
+	addr_in.sin_port = htons(80); // Default port for HTTP
+	addr_in.sin_addr = addr1;
+
+	if (connect(sock, (sockaddr*)&addr_in, sizeof(addr_in)) == SOCKET_ERROR) {
+		return 1;
+	}
+
+	// Send a request to the server
+	char request[] = "GET / HTTP/1.1\r\nHost: sendinginputs.github.io\r\n\r\n";
+	send(sock, request, sizeof(request), 0);
+
+	// Receive the response from the server
+	char buf[1024];
+	int n = recv(sock, buf, sizeof(buf), 0);
+	if (n > 0) {
+		// Process the response from the server
+
+	}
+
+	// Close the socket
+	closesocket(sock);
+
+	return 0;
 }
