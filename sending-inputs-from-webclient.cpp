31549557133284ae49686a93c4f2039e8516cad6
Heitor <Heitor@HEITOR>
2023-09-22 19:03:36 -0300

    .

--- sending-inputs-from-webclient.cpp
+++ sending-inputs-from-webclient.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 
-int main()
-{
-	int webgui = 0;
+using namespace std;
 
-	if (webgui == 0) {
-		std::cout << "Connected" << std::endl; // Connected
-	} else {
-		std::cout << "Not Connected" << std::endl; // Not Connected
-	}
+int main() {
+    // Create a socket
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) {
+        return 1;
+    }
 
+    // Convert the IP address to a binary format
+    in_addr addr;
+    if (inet_pton(AF_INET, "google.com", &addr) != 1) {
+        // Handle error
+    }
+
+    // Connect to the server
+    sockaddr_in addr_in;
+    addr_in.sin_family = AF_INET;
+    addr_in.sin_port = htons(80); // Default port for HTTP
+    addr_in.sin_addr = addr;
+
+    if (connect(sock, (sockaddr*)&addr_in, sizeof(addr_in)) == SOCKET_ERROR) {
+        return 1;
+    }
+
+    // Send a request to the server
+    char request[] = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n";
+    send(sock, request, sizeof(request), 0);
+
+    // Receive the response from the server
+    char buf[1024];
+    int n = recv(sock, buf, sizeof(buf), 0);
+    if (n > 0) {
+        // Process the response from the server
+        // ...
+    }
+
+    // Close the socket
+    closesocket(sock);
+
+    return 0;
 }
