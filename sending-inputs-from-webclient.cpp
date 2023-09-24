0aec50f1afe85b43a057334c00e0e40cc7ad77d4
Heitor <Heitor@HEITOR>
2023-09-24 14:39:20 -0300

    .

--- sending-inputs-from-webclient.cpp
+++ sending-inputs-from-webclient.cpp
@@ -37,25 +37,6 @@ int websocket()
 		}
 	}
 
-	// Convert the IP address to a binary format
-	in_addr addr1;
-	if (inet_pton(AF_INET, "ws://heitorrosa.github.io/sending-inputs-from-webclient", &addr1) != 1) {
-	}
-
-	// Connect to the server
-	sockaddr_in addr_in;
-	addr_in.sin_family = AF_INET;
-	addr_in.sin_port = htons(80); // Default port for HTTP
-	addr_in.sin_addr = addr1;
-
-	if (connect(sock, (sockaddr*)&addr_in, sizeof(addr_in)) == SOCKET_ERROR) {
-		return 1;
-	}
-
-	// Send a request to the server
-	char request[] = "GET / HTTP/1.1\r\nHost: ws://heitorrosa.github.io/sending-inputs-from-webclient\r\n\r\n";
-	send(sock, request, sizeof(request), 0);
-
 	// Close the socket
 	closesocket(sock);
 
