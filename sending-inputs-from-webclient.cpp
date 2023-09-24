79a31a8de29a8123e90e0cf5d33166191e6f3c6c
Heitor <Heitor@HEITOR>
2023-09-24 15:17:14 -0300

    .

--- sending-inputs-from-webclient.cpp
+++ sending-inputs-from-webclient.cpp
@@ -5,46 +5,55 @@
 
 using namespace std;
 
-int websocket()
-{
-	// Starts Winsock
-	WSADATA wsaData;
-	WSAStartup(MAKEWORD(2, 2), &wsaData);
+int websocket() {
+    // Starts Winsock
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	// Create a WebSocket server socket
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock == INVALID_SOCKET) {
-		return 1;
-	}
+    // Create a WebSocket server socket
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) {
+        return 1;
+    }
 
-	// Bind the socket to a port
-	sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(80);
-	addr.sin_addr.s_addr = INADDR_ANY;
+    // Bind the socket to a port
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(80);
+    addr.sin_addr.s_addr = INADDR_ANY;
 
-	bind(sock, (sockaddr*)&addr, sizeof(addr));
+    bind(sock, (sockaddr*)&addr, sizeof(addr));
 
-	// Listen for incoming connections
-	listen(sock, SOMAXCONN);
+    // Listen for incoming connections
+    listen(sock, SOMAXCONN);
 
-	// Start the WebSocket server loop
-	while (true) {
-		// Accept incoming connections
-		SOCKET client_sock = accept(sock, NULL, NULL);
-		if (client_sock == INVALID_SOCKET) {
-			break;
-		}
-	}
+    // Accept an incoming connection
+    SOCKET client_sock = accept(sock, NULL, NULL);
+    if (client_sock == INVALID_SOCKET) {
+        return 1;
+    }
 
-	// Close the socket
-	closesocket(sock);
+    // Close the server socket
+    closesocket(sock);
 
-	return 0;
+    // Send a message to the client
+    const char* message = "localhost";
+    send(client_sock, message, strlen(message), 0);
+
+    // Receive a message from the client
+    char buffer[1024];
+    int bytes_received = recv(client_sock, buffer, sizeof(buffer), 0);
+
+    // Close the client socket
+    closesocket(client_sock);
+
+    WSACleanup();
+
+    return 0;
 }
 
 int main() {
-	websocket();
+    websocket();
 
-	return 0;
+    return 0;
 }
